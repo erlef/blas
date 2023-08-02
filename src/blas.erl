0b02e358b1f2e505a769b771024c55f65b107c38
tanguyl <losseautanguy@gmail.com>
2023-08-02 18:59:34 +0200

    Trying to write benchmarks.

--- src/blas.erl
+++ src/blas.erl
@@ -1,7 +1,7 @@
 -module(blas).
 
 -export([run/1, run/2, hash/1]).
--export([new/1, new/2, shift/2, copy/2, to_bin/1, to_bin/2, to_list/2, ltb/2, btl/2]).
+-export([new/1, new/2, shift/2, copy/2, to_bin/1, to_bin/2, to_list/2, ltb/2, btl/2, benchmark/0, benchmark/1]).
 
 
 -record(c_binary, {size, offset, resource}).
@@ -71,3 +71,43 @@ dirty_unwrapper(_) -> nif_not_loaded.
 clean_unwrapper(_) -> nif_not_loaded.
 hash(_)->nif_not_loaded.
 
+
+benchmark(Size) ->
+    GenMatrix = fun(N) -> 
+        blas:new(d, [rand:uniform(20) || _ <- lists:seq(1,N*N)])
+    end,
+    GenTuple = fun(N) ->
+        {dgemm, blasColMajor, blasNoTrans, blasNoTrans, N,N,N, 1.0, GenMatrix(N), N, GenMatrix(N), N, 1.0, GenMatrix(N), N}
+    end,
+
+    Tuple = GenTuple(Size),
+    {T, ok} = timer:tc(blas, run, [Tuple, clean]),
+    T.
+
+normsum(L)->
+    % Do a weighed normalised sum of the elements of a list, with weigths: 1, ... , 0.1
+    N_elems = length(L),
+    if 
+        N_elems == 1 ->
+            lists:nth(1,L);
+        true ->
+            Weigths = [I/N_elems || I <- lists:seq(N_elems, 1, -1)],
+            Sum     = lists:foldl(
+                fun({E,W}, S) ->
+                    S + E/W 
+                end,
+                0,
+                lists:zip(L, Weigths)
+            ),
+            round(Sum / lists:foldl(fun(V,A)-> V+A end, 0, Weigths))
+    end.
+
+benchmark()->
+    % Reach 1ms.
+    Iterate = fun(N)->
+        T = benchmark(N),
+        io:format("Time: ~w ~n", [T]),
+        round(max(min(10, 1000.0/T), 0.1) * N)
+    end,
+
+    lists:foldl(fun (_, L) -> [Iterate(normsum(L))|L] end, [Iterate(20)], lists:seq(1, 20)).
